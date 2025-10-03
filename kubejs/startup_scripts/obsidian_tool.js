051415263616b340fc58a73fe99f6e29597d9618
yuitayuun <wojtek@int.pl>
2025-10-03 12:46:19 +0200

    123123123

---
+++ kubejs/startup_scripts/obsidian_tool.js
@@ -0,0 +1,16 @@
+ItemEvents.modification(event => {
+    event.modify('obsidianequipment:obsidian_pickaxe', item => {
+        item.digSpeed = 12
+    })})
+ItemEvents.modification(event => {
+    event.modify('obsidianequipment:obsidian_shovel', item => {
+        item.digSpeed = 12
+    })})
+ItemEvents.modification(event => {
+    event.modify('obsidianequipment:obsidian_axe', item => {
+        item.digSpeed = 12
+    })})
+ItemEvents.modification(event => {
+    event.modify('obsidianequipment:obsidian_sword', item => {
+        item.attackDamage = 7.5
+})})
